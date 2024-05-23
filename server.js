ccff5efc5921845a9fb2d35cdae49d375972ff27
Aditya Chaturvedi <aditya.sc@nic.in>
2024-05-23 14:38:47 +0530

    added .env

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const forumsRoute = require('./routes/forums');
+const dotenv = require('dotenv');
 
 const app = express();
+
+dotenv.config({ path: './config.env' });
+
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 mongoose
-  .connect('', {
+  .connect(process.env.DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
